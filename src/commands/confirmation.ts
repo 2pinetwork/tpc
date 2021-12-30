f3cf7e752272ee76a7e156dd38787765b312902b
Batudo <batudo@2pi.network>
2021-12-30 20:30:30 -0300

    Let's separate data from labels with a colon =)

--- src/commands/confirmation.ts
+++ src/commands/confirmation.ts
@@ -9,16 +9,16 @@ const processConfirmation = async (options: OptionValues) => {
   const wallet        = key && (await getWallet(key, walletOptions, devMode))
 
   if (key) {
-    console.log('Your API key', key.key)
-    console.log('Your API secret', key.secret)
+    console.log('Your API key:', key.key)
+    console.log('Your API secret:', key.secret)
   }
 
   if (wallet?.address) {
-    console.log('Your wallet public address', wallet.address)
+    console.log('Your wallet public address:', wallet.address)
   }
 
   if (wallet?.mnemonic) {
-    console.log('Your Mnemonic', wallet.mnemonic)
+    console.log('Your Mnemonic:', wallet.mnemonic)
     console.log('(Make sure you write down and keep your mnemonic in a safe place. We don\'t keep record of it anywhere.)')
   }
 }
--- src/commands/createRandomWallet.ts
+++ src/commands/createRandomWallet.ts
@@ -3,9 +3,9 @@ import { createRandomWallet } from '../wallet'
 const processCreateRandomWallet = () => {
   const { address, mnemonic } = createRandomWallet()
 
-  console.log('Your wallet public address', address)
+  console.log('Your wallet public address:', address)
 
-  console.log('Your Mnemonic', mnemonic.phrase)
+  console.log('Your Mnemonic:', mnemonic.phrase)
   console.log('(Make sure you write down and keep your mnemonic in a safe place. We don\'t keep record of it anywhere.)')
 }
 
